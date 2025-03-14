3e0f9a9d37998bbb448817bfa78bd571c05430cd
Yilin Han <105486492+hanyilinn@users.noreply.github.com>
2025-03-14 15:18:31 +0800

    Create classcification_model.py

---
+++ model/classcification_model.py
@@ -0,0 +1,275 @@
+# This is the networks script
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TSception(nn.Module):
+    def conv_block(self, in_chan, out_chan, kernel, step, pool):
+        return nn.Sequential(
+            nn.Conv2d(in_channels=in_chan, out_channels=out_chan,
+                      kernel_size=kernel, stride=step),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(kernel_size=(1, pool), stride=(1, pool)))
+
+    def __init__(self, num_classes, input_size, sampling_rate, num_T, num_S, hidden, dropout_rate):
+        # input_size: 1 x EEG channel x datapoint
+        super(TSception, self).__init__()
+        self.inception_window = [0.5, 0.25, 0.125]
+        self.pool = 8
+        # by setting the convolutional kernel being (1,lenght) and the strids being 1 we can use conv2d to
+        # achieve the 1d convolution operation
+        self.Tception1 = self.conv_block(1, num_T, (1, int(self.inception_window[0] * sampling_rate)), 1, self.pool)
+        self.Tception2 = self.conv_block(1, num_T, (1, int(self.inception_window[1] * sampling_rate)), 1, self.pool)
+        self.Tception3 = self.conv_block(1, num_T, (1, int(self.inception_window[2] * sampling_rate)), 1, self.pool)
+
+        self.Sception1 = self.conv_block(num_T, num_S, (int(input_size[1]), 1), 1, int(self.pool*0.25))
+        self.Sception2 = self.conv_block(num_T, num_S, (int(input_size[1] * 0.5), 1), (int(input_size[1] * 0.5), 1),
+                                         int(self.pool*0.25))
+        self.fusion_layer = self.conv_block(num_S, num_S, (3, 1), 1, 4)
+        self.BN_t = nn.BatchNorm2d(num_T)
+        self.BN_s = nn.BatchNorm2d(num_S)
+        self.BN_fusion = nn.BatchNorm2d(num_S)
+
+        self.fc = nn.Sequential(
+            nn.Linear(num_S, hidden),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden, num_classes)
+        )
+
+    def forward(self, x):
+        y = self.Tception1(x)
+        out = y
+        y = self.Tception2(x)
+        out = torch.cat((out, y), dim=-1)
+        y = self.Tception3(x)
+        out = torch.cat((out, y), dim=-1)
+        out = self.BN_t(out)
+        z = self.Sception1(out)
+        out_ = z
+        z = self.Sception2(out)
+        out_ = torch.cat((out_, z), dim=2)
+        out = self.BN_s(out_)
+        out = self.fusion_layer(out)
+        out = self.BN_fusion(out)
+        out = torch.squeeze(torch.mean(out, dim=-1), dim=-1)
+        out = self.fc(out)
+        return out
+
+class TSceptionIJCNN(nn.Module):
+    """
+    Y. Ding et al., "TSception:A Deep Learning Framework for Emotion Detection Using EEG,"
+    2020 International Joint Conference on Neural Networks (IJCNN), Glasgow, UK, 2020, pp. 1-7,
+    doi: 10.1109/IJCNN48605.2020.9206750.
+    """
+    def conv_block(self, in_chan, out_chan, kernel, step, pool):
+        return nn.Sequential(
+            nn.Conv2d(in_channels=in_chan, out_channels=out_chan,
+                      kernel_size=kernel, stride=step, padding=0),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(kernel_size=(1, pool), stride=(1, pool)))
+
+    def __init__(self, num_classes, input_size, sampling_rate, num_T, num_S, hidden, dropout_rate):
+        # input_size: 1 x EEG channel x datapoint
+        super(TSceptionIJCNN, self).__init__()
+        self.inception_window = [0.5, 0.25, 0.125]
+        self.pool = 8
+        # by setting the convolutional kernel being (1,lenght) and the strids being 1 we can use conv2d to
+        # achieve the 1d convolution operation
+        self.Tception1 = self.conv_block(1, num_T, (1, int(self.inception_window[0] * sampling_rate)), 1, self.pool)
+        self.Tception2 = self.conv_block(1, num_T, (1, int(self.inception_window[1] * sampling_rate)), 1, self.pool)
+        self.Tception3 = self.conv_block(1, num_T, (1, int(self.inception_window[2] * sampling_rate)), 1, self.pool)
+
+        self.Sception1 = self.conv_block(num_T, num_S, (int(input_size[-2]), 1), 1, int(self.pool*0.25))
+        self.Sception2 = self.conv_block(num_T, num_S, (int(input_size[-2] * 0.5), 1), (int(input_size[-2] * 0.5), 1),
+                                         int(self.pool*0.25))
+        self.BN_t = nn.BatchNorm2d(num_T)
+        self.BN_s = nn.BatchNorm2d(num_S)
+
+        size = self.get_size(input_size)
+        self.fc = nn.Sequential(
+            nn.Linear(size[1], hidden),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden, num_classes)
+        )
+
+    def forward(self, x):
+        y = self.Tception1(x)
+        out = y
+        y = self.Tception2(x)
+        out = torch.cat((out, y), dim=-1)
+        y = self.Tception3(x)
+        out = torch.cat((out, y), dim=-1)
+        out = self.BN_t(out)
+        z = self.Sception1(out)
+        out_ = z
+        z = self.Sception2(out)
+        out_ = torch.cat((out_, z), dim=2)
+        out = self.BN_s(out_)
+        out = out.view(out.size()[0], -1)
+        out = self.fc(out)
+        return out
+
+    def get_size(self, input_size):
+        # here we use an array with the shape being
+        # (1(mini-batch),1(convolutional channel),EEG channel,time data point)
+        # to simulate the input data and get the output size
+        data = torch.ones((1, 1, input_size[-2], int(input_size[-1])))
+        y = self.Tception1(data)
+        out = y
+        y = self.Tception2(data)
+        out = torch.cat((out, y), dim=-1)
+        y = self.Tception3(data)
+        out = torch.cat((out, y), dim=-1)
+        out = self.BN_t(out)
+        z = self.Sception1(out)
+        out_final = z
+        z = self.Sception2(out)
+        out_final = torch.cat((out_final, z), dim=2)
+        out = self.BN_s(out_final)
+        out = out.view(out.size()[0], -1)
+        return out.size()
+
+
+
+
+
+# %% Shallow convnet
+class shallowConvNet(nn.Module):
+    def convBlock(self, inF, outF, dropoutP, kernalSize, *args, **kwargs):
+        return nn.Sequential(
+            nn.Dropout(p=dropoutP),
+            Conv2dWithConstraint(inF, outF, kernalSize, bias=False, max_norm=2, *args, **kwargs),
+            nn.BatchNorm2d(outF),
+            nn.ELU(),
+            nn.MaxPool2d((1, 3), stride=(1, 3))
+        )
+
+    def firstBlock(self, outF, dropoutP, kernalSize, nChan, *args, **kwargs):
+        return nn.Sequential(
+            Conv2dWithConstraint(1, outF, kernalSize, padding=0, max_norm=2, *args, **kwargs),
+            Conv2dWithConstraint(40, 40, (nChan, 1), padding=0, bias=False, max_norm=2),
+            nn.BatchNorm2d(outF),
+        )
+
+    def calculateOutSize(self, nChan, nTime):
+        '''
+        Calculate the output based on input size.
+        model is from nn.Module and inputSize is a array.
+        '''
+        data = torch.rand(1, 1, nChan, nTime)
+        block_one = self.firstLayer
+        avg = self.avgpool
+        dp = self.dp
+        out = torch.log(block_one(data).pow(2))
+        out = avg(out)
+        out = dp(out)
+        out = out.view(out.size()[0], -1)
+        return out.size()
+
+    def __init__(self, nChan, nTime, nClass=3, dropoutP=0.25, *args, **kwargs):
+        super(shallowConvNet, self).__init__()
+
+        kernalSize = (1, 25)
+        nFilt_FirstLayer = 40
+
+        self.firstLayer = self.firstBlock(nFilt_FirstLayer, dropoutP, kernalSize, nChan)
+        self.avgpool = nn.AvgPool2d((1, 75), stride=(1, 15))
+        self.dp = nn.Dropout(p=dropoutP)
+        self.fSize = self.calculateOutSize(nChan, nTime)
+        self.lastLayer = nn.Linear(self.fSize[-1], nClass)
+
+    def forward(self, x):
+        x = self.firstLayer(x)
+        x = torch.log(self.avgpool(x.pow(2)))
+        x = self.dp(x)
+        x = x.view(x.size()[0], -1)
+        x = self.lastLayer(x)
+
+        return x
+
+
+class Conv2dWithConstraint(nn.Conv2d):
+    def __init__(self, *args, doWeightNorm=True, max_norm=1, **kwargs):
+        self.max_norm = max_norm
+        self.doWeightNorm = doWeightNorm
+        super(Conv2dWithConstraint, self).__init__(*args, **kwargs)
+
+    def forward(self, x):
+        if self.doWeightNorm:
+            self.weight.data = torch.renorm(
+                self.weight.data, p=2, dim=0, maxnorm=self.max_norm
+            )
+        return super(Conv2dWithConstraint, self).forward(x)
+
+
+class LinearWithConstraint(nn.Linear):
+    def __init__(self, *args, doWeightNorm=True, max_norm=1, **kwargs):
+        self.max_norm = max_norm
+        self.doWeightNorm = doWeightNorm
+        super(LinearWithConstraint, self).__init__(*args, **kwargs)
+
+    def forward(self, x):
+        if self.doWeightNorm:
+            self.weight.data = torch.renorm(
+                self.weight.data, p=2, dim=0, maxnorm=self.max_norm
+            )
+        return super(LinearWithConstraint, self).forward(x)
+
+
+class ConvNet(nn.Module):
+    def __init__(self, num_classes):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5, stride=(1, 1), padding=(2, 2), bias=True),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=5, stride=(1, 1), padding=(2, 2), bias=True),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=(1, 1), padding=(1, 1), bias=True),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+        self.fc1 = nn.Linear(128 * 7 * 100, 256, bias=True) # 这里的 100 对应800除以 8 ，即 2 的 3 次方，平均池化 3 次
+        self.fc2 = nn.Linear(256, num_classes, bias=True)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = out.reshape(out.size(0), -1)
+        # print(out.shape)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+    
+if __name__ == "__main__":
+    num_classes = 3
+    batch_size = 24
+    input = torch.ones(batch_size, 1, 62, 800)
+    # model = ConvNet(num_classes)
+    
+    input_size = (1, 62, 800)  # Input shape (1 channel, 62 channels, 800 data points)
+    sampling_rate = 200  # Assuming the sampling rate is 256 Hz (adjust as needed)
+    num_T = 32  # Example number of temporal filters (adjust based on your experiment)
+    num_S = 64  # Example number of spatial filters (adjust based on your experiment)
+    hidden = 256  # Example number of hidden units in the fully connected layer
+    dropout_rate = 0.5  # Dropout rate for regularization
+    # model = TSception(num_classes, input_size, sampling_rate, num_T, num_S, hidden, dropout_rate)
+    
+    model = eegNet(nChan=62, nTime=800)
+    # model = shallowConvNet(nChan=62, nTime=800)
+    # model = deepConvNet(nChan=62, nTime=800)
+    
+    output = model(input)
+    print("Shape of output: ", output.shape)
